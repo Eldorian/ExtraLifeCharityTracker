90da88826460d59b3463653134b9d209feb61811
James R Cook <jer_cook79@hotmail.com>
2018-03-28 19:37:55 -0500

    Setting up initial API

---
+++ ExtraLife.API/Controllers/ExtraLifeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using ExtraLife.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExtraLife.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ExtraLifeController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int participantId)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = 
+                await client.GetAsync(
+                    $"https://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID={participantId}&format=json");
+
+            response.EnsureSuccessStatusCode();
+
+             var participant = response.Content.ReadAsJsonAsync<ParticipantModel>();
+            return Ok(participant);
+        }
+    }
+}
---
+++ ExtraLife.API/Models/ParticipantModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtraLife.API.Models
+{
+    public class ParticipantModel
+    {
+        public string DisplayName { get; set; }
+        public decimal TotalRaisedAmount { get; set; }
+        public decimal FundraisingGoal { get; set; }
+        public int ParticipantId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string AvatarUrl { get; set; }
+        public int TeamId { get; set; }
+        public bool IsTeamCaptain { get; set; }
+
+    }
+}
